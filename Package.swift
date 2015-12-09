f34f66841f6463e50704179898231b4d2433e861
Dan Appel <dan.appel00@gmail.com>
2015-12-08 21:37:19 -0800

    Fix typo

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "CMongoC",
     dependencies: [
-        .Package(url: "https://github.com/Danappelxx/CLibbson.git", major: 1, minor: 3)
+        .Package(url: "https://github.com/Danappelxx/CLibbson.git", majorVersion: 1, minor: 3)
     ]
 )
