1f8c3b7ee9674da28f9e2c30d0067df60af2311d
Dan Appel <Dan.appel00@gmail.com>
2015-12-10 17:57:20 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,1 @@
-import PackageDescription
 
-let package = Package(
-    name: "CMongoC",
-    dependencies: [
-        .Package(url: "https://github.com/Danappelxx/CLibbson.git", majorVersion: 0, minor: 1)
-    ]
-)
