3f58eb9b4d9449d361980e67c82af7c0b4177470
Jimmy Arts <arts.jimmy@gmail.com>
2016-10-14 13:40:56 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,8 +2,8 @@ import PackageDescription
 
 let package = Package(
     name: "CMongoC",
+    pkgConfig: "libmongoc-1.0",
     dependencies: [
         .Package(url: "https://github.com/Danappelxx/CLibbson.git", majorVersion: 0, minor: 1)
-    ],
-    pkgConfig: "libmongoc-1.0"
+    ]
 )
